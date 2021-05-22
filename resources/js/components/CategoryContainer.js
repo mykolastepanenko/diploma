531905fdada48d62f218194747a264e0aafb1c68
mykolastepanenko <Nikolaua36@gmail.com>
2021-05-22 12:22:25 +0300

    categories slider added

--- resources/js/components/CategoryContainer.js
+++ resources/js/components/CategoryContainer.js
@@ -1,21 +1,62 @@
 import React from "react";
 import ItemCategory from "./ItemCategory";
+import Slider from "react-slick";
 
 function CategoryContainer(props) {
+    const settings = {
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
+
     return (
         <>
             <div className={"row"}>
                 <div className="col">
-                    <ItemCategory category={props.categories[0]} img={props.img} />
-                </div>
-                <div className="col">
-                    <ItemCategory category={props.categories[1]} img={props.img} />
-                </div>
-                <div className="col">
-                    <ItemCategory category={props.categories[2]} img={props.img} />
-                </div>
-                <div className="col">
-                    <ItemCategory category={props.categories[3]} img={props.img} />
+                    <Slider {...settings}>
+                        <ItemCategory
+                            category={props.categories[0]}
+                            img={props.img}
+                        />
+                        <ItemCategory
+                            category={props.categories[1]}
+                            img={props.img}
+                        />
+                        <ItemCategory
+                            category={props.categories[2]}
+                            img={props.img}
+                        />
+                        <ItemCategory
+                            category={props.categories[3]}
+                            img={props.img}
+                        />
+                        <ItemCategory
+                            category={props.categories[4]}
+                            img={props.img}
+                        />
+                    </Slider>
                 </div>
             </div>
         </>
